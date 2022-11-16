4162a9f5c80a582da9d97deac7cee8aae8aa8d0a
Sebastian Sanchez Ramirez <100661054+sesanchezra@users.noreply.github.com>
2022-11-16 14:24:26 -0500

    Rutas acordadas - Inicio de vinculacion firebase

--- healthyApp/src/App.jsx
+++ healthyApp/src/App.jsx
@@ -6,27 +6,30 @@ import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
 import Cart from './components/Cart/Cart'
 import Profile from './components/Profile/Profile'
 import SignUp from './components/SignUp/SignUp'
-import Products from './components/Products/Products'
-import Recipes from './components/Recipes/Recipes'
+import ProductDetail from './components/Products/ProductDetail'
+import RecipeDetail from './components/Recipes/RecipeDetail'
+import { AuthProvider } from "./context/AuthContext";
 
 
 function App() {
 
   return (
     <div className="App">
-      <Routes >
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/' element={<Home />}/>
-        <Route path='/products' element={<Products />}/>
-        <Route path='/recipes' element={<Recipes />} />
+      <AuthProvider>
+        <Routes >
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetail />} />
+          <Route path='/recipe/:id' element={<RecipeDetail />} />
 
-        <Route element={<ProtectedRoutes />}>
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/profile' element={<Profile />} />
-        </Route>
+          <Route element={<ProtectedRoutes />}>
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/profile' element={<Profile />} />
+          </Route>
 
-      </Routes>
+        </Routes>
+      </AuthProvider>
     </div>
   )
 }
--- healthyApp/src/components/Login/Login.jsx
+++ healthyApp/src/components/Login/Login.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import '../Login/Login.css'
 
 const Login = () => {
     const navigate = useNavigate()
 
+    const { handleSubmit, register, reset } = useForm()
+
     const loginSubmit = () => {
-        localStorage.setItem('token','true')
+        localStorage.setItem('token', 'true')
         navigate('/')
-    } 
+    }
 
     return (
-        <div>
-            <button onClick={loginSubmit}>
-                Login
-            </button>
+        <div className='Login'>
+            <form className='Login__form'>
+                <div className='Login__form__simple'>
+                    <div className='Login__form__simple__input'>
+                        <label htmlFor="email" className=''>Email</label>
+                        <input type="text" className='' placeholder='email@company.com'/>
+                    </div>
+                    <div className='Login__form__simple__input'>
+                        <label htmlFor="password" className=''>Password</label>
+                        <input type="password" className='' placeholder='* * * * * *'/>
+                    </div>
+                    <button onClick={loginSubmit} className='Login__form__simple__btn'>
+                        Login
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
