839936b9bf19feb4dc484d2b728c029b68a8968a
Ana Juárez <lisa13suarez@gmail.com>
2022-11-18 14:45:46 -0600

    agregando el userForm - register

--- healthyApp/src/components/SignUp/SignUp.jsx
+++ healthyApp/src/components/SignUp/SignUp.jsx
@@ -11,13 +11,16 @@ import Form from 'react-bootstrap/Form'
 const SignUp = () => {
     const navigate = useNavigate()
     
-    const { handleSubmit } = useForm()
+    const { handleSubmit, register } = useForm()
 
-    const submit = () => navigate('/login')
+    const onSubmit = (data) => {
+        navigate('/login')
+        console.log(data);
+    }
 
     return (
         <>
-        <Form  className='p-3' onSubmit={handleSubmit(submit)}>
+        <Form  className='p-3' onSubmit={handleSubmit(onSubmit)}>
             <div className='header_Form'>
                 <h1 className='text-start'> Bienvenido! </h1>
                 <div className='div_img__user m-auto'>
@@ -25,32 +28,32 @@ const SignUp = () => {
                 </div>
             </div>
 
-            <Form.Group className='mt-3' controlId='formBasicEmail'>
+            <Form.Group className='mt-3' controlId='formBasicName'>
                 <Form.Label className='form_label'> Nombre </Form.Label>
-                <Form.Control className='input' type='text' placeholder='Input Text'/>
+                <Form.Control className='input' type='text' placeholder='Input Text' {...register("name")}/>
             </Form.Group>
 
-            <Form.Group className='mt-3' controlId='formBasicPassword'>
+            <Form.Group className='mt-3' controlId='formBasicLastName'>
                 <Form.Label className='form_label'> Apellido </Form.Label>
-                <Form.Control className='input' type='password' placeholder='Input Text'/>
+                <Form.Control className='input' type='text' placeholder='Input Text' {...register('last_name')}/>
             </Form.Group>
 
-            <Form.Group className='mt-3' controlId='formBasicPassword'>
+            <Form.Group className='mt-3' controlId='formBasicEmail'>
                 <Form.Label className='form_label'> Mail </Form.Label>
-                <Form.Control className='input' type='mail' placeholder='Input Text'/>
+                <Form.Control className='input' type='mail' placeholder='Input Text' {...register('mail')}/>
             </Form.Group>
 
-            <Form.Group className='mt-3' controlId='formBasicPassword'>
+            <Form.Group className='mt-3' controlId='formBasicRegisterPassword'>
                 <Form.Label className='form_label'> Contraseña </Form.Label>
-                <Form.Control className='input' type='password' placeholder='* * * *'/>
+                <Form.Control className='input' type='password' placeholder='* * * *' {...register('password')}/>
             </Form.Group>
 
-            <Form.Group className='mt-3' controlId='formBasicPassword'>
+            <Form.Group className='mt-3' controlId='formBasicVerifyPassword'>
                 <Form.Label className='form_label'> Verifique Contraseña </Form.Label>
-                <Form.Control className='input' type='password' placeholder='* * * *'/>
+                <Form.Control className='input' type='password' placeholder='* * * *' {...register('verifyPss')}/>
             </Form.Group>
 
-            <Button className='btn mt-4' type='submit'> Registrarme </Button>
+            <input className='btn mt-4' type='submit' value="Registrate"/>
         </Form>
         </>
     )
