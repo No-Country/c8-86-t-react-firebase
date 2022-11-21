b30d4d118a10be3f7f11c82aedb1e3c8fd03d275
Diego Gonzalez <90977032+DiegoNGonzalez@users.noreply.github.com>
2022-11-21 12:29:37 -0300

    update protected routes

--- healthyApp/src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ healthyApp/src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoutes = () => {
+export function ProtectedRouted({ children }) {
+    const { user, loading } = useAuth();
 
-    if(localStorage.getItem('token')==='true'){
-        return <Outlet />
-    }
-    else{
-        return <Navigate to='/login'/>
-    }
-    
+    if (loading) return <h1>Loading</h1>;
+    if (!user) return <Navigate to='/login' />;
+    return <>{children}</>;
 }
-
-export default ProtectedRoutes
