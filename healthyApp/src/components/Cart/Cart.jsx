5db4a707837d30c725293dd00513acd8793c65c7
Ana Juárez <lisa13suarez@gmail.com>
2022-12-03 13:05:53 -0600

    añadiendo estilos al cart

--- healthyApp/src/components/Cart/Cart.jsx
+++ healthyApp/src/components/Cart/Cart.jsx
@@ -1,10 +1,70 @@
 import React from 'react'
+import '../Header/Header.css'
+import './Cart.css'
+import Container from 'react-bootstrap/Container'
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Cart = () => {
+
+    const [detailProduct, setDetailProduct] = useState({})
+
+    useEffect(() => {
+        axios.get(`https://us-central1-saine-api.cloudfunctions.net/app/api/products/1`).then(data => setDetailProduct(data.data))
+    }, []);
+
     return (
-        <div>
-            Cart
-        </div>
+        <>
+            <div className='Header'>
+                <button  className={`Header__btn-false left`}> <AiOutlineMenu /> </button>
+                <button className={`Header__btn-false right opacity`}> <AiOutlineShoppingCart /></button>
+            </div>
+
+            <Container>
+                <h2 className='title_cart mt-3 ms-3'>Mi Carrito</h2>
+
+                <div className='listInCart'>
+                <div className='listInCart__Item'>
+                        <div className='addImg'>
+                            <div className='containerImage'>
+                                <img src={detailProduct.imageUrl} alt="" />
+                            </div>
+                                <ul>
+                                    <li>item</li>
+                                    <li>precio</li>
+                                </ul>
+                        </div>
+                        <div className='addCount'>
+                            <div className='inputs'>
+                                <button>-</button>
+                                <input type="text" value={2} />
+                                <button>+</button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className='listInCart__Item'>
+                        <div className='addImg'>
+                            <div className='containerImage'>
+                                <img src={detailProduct.imageUrl} alt="" />
+                            </div>
+                                <ul>
+                                    <li>item</li>
+                                    <li>precio</li>
+                                </ul>
+                        </div>
+                        <div className='addCount'>
+                            <div className='inputs'>
+                                <button>-</button>
+                                <input type="text" value={2} />
+                                <button>+</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        </>
     )
 }
 
