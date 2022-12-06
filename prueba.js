a84e10e523cbe0ae7082d1037fb1691964d39386
Sebastian Sanchez Ramirez <100661054+sesanchezra@users.noreply.github.com>
2022-12-06 13:57:08 -0500

    Editar perfil funcionalidad 80%

--- prueba.js
+++ prueba.js
@@ -8,3 +8,8 @@ let array1 = string1.split(' ')
 let array2 = string2.split(' ')
 
 console.log(string2.includes(string1))
+
+emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+console.log(emailRegex.test('sebastian@gmail.com'))
+console.log(emailRegex.test('sebastian@gmail.com'))
