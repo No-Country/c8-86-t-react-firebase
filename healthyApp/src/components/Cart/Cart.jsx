0edb4b91f860af32a179e5a5ce7a9852dd74264d
Ana Juárez <lisa13suarez@gmail.com>
2022-12-07 00:03:55 -0600

    probando funcionalidad

--- healthyApp/src/components/Cart/Cart.jsx
+++ healthyApp/src/components/Cart/Cart.jsx
@@ -5,24 +5,40 @@ import Container from 'react-bootstrap/Container'
 import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Header from '../Header/Header'
-
+import Arrow from '../../assets/arrow-back.svg'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
-
+    const navigate = useNavigate()
     const [detailProduct, setDetailProduct] = useState({})
     const [checkCant, setCheckCant] = useState(1)
 
+    const url = `https://us-central1-saine-api.cloudfunctions.net/app/api/products/`;
+
     useEffect(() => {
         setCheckCant(1)
-        axios.get(`https://us-central1-saine-api.cloudfunctions.net/app/api/products/2`).then(data => setDetailProduct(data.data))
+
+        const l = JSON.parse(localStorage.getItem('addProduct'));
+        console.log(JSON.parse(localStorage.getItem('addProduct')));
+        const arr = [];
+        arr.push(l.id);
+        console.log(l.id);
+        /*arr.forEach(i => {
+            axios.get(`https://us-central1-saine-api.cloudfunctions.net/app/api/products/${i.id}`).then(data => setDetailProduct(data.data))
+        });*/
     },[]);
 
-    console.log(checkCant);
+    console.log(detailProduct);
 
     return (
         <>
-            <Header/>
+            <header className='header-menu d-flex justify-content-start align-items-center'>
+                <div>
+                    <button className='border border-0' onClick={() => navigate('/')}> 
+                        <img src={Arrow} alt="arrow back" />
+                    </button>
+                </div>
+            </header>
             <Container>
                 <h2 className='title_cart mt-3 ms-3'>Mi Carrito</h2>
 
@@ -34,8 +50,8 @@ const Cart = () => {
                                 <img src={detailProduct.imageUrl}alt="" />
                             </div>
                                 <ul>
-                                    <li>Lorem ipsum</li>
-                                    <li>PRECIO</li>
+                                    <li>{detailProduct.name}</li>
+                                    <li><b> $ {detailProduct.price}</b></li>
                                 </ul>
                         </div>
                             <div className='addCount'>
